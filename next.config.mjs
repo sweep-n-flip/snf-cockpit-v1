1297d13aa03752e9209b63f30a2dc810b7fbf70e
gervickasjs <wellitogervickas@gmail.com>
2024-07-05 16:08:26 -0300

    fix: remove workflow

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NODE_ENV == 'production' ? 'standalone' : undefined,
   reactStrictMode: true,
   swcMinify: true,
 }
